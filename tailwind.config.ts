a7cc5473c205f7bf1ede7f237ef96b9bd96f3e11
Tom Barta <thomas.barta@gmail.com>
2025-02-19 16:10:14 -0700

    wip bootstrapping

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyUI from 'daisyui'
 
 export default {
   content: [
@@ -14,5 +15,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyUI],
 } satisfies Config;
